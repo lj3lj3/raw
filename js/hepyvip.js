e50571659fb297d1749deaaf1bdff1340e05c788
Daryl Liu <lj3lj3@gmail.com>
2023-12-02 21:30:20 +0800

    Update hepyvip.js

--- js/hepyvip.js
+++ js/hepyvip.js
@@ -3,9 +3,10 @@ if (!$response.body) {
     return;
 }
 
-var body = JSON.parse($response.body)
-body.result.pvcstatus = 2;
-body.result.pvcversion = 2;
+//var body = JSON.parse($response.body)
+//body.result.pvcstatus = 2;
+//body.result.pvcversion = 2;
+var body = "";
 
 $done({
     body: JSON.stringify(body)
